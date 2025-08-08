3cd5088bfbff3f87470c1374bb31dbae2462d7b4
queency581 <estellastephens20@gmail.com>
2025-08-08 17:19:43 +0300

    added new code

---
+++ python/app.py
@@ -0,0 +1,18 @@
+print("hello world")
+import math
+
+def calculator():
+    print("Welcome to the Python Switch-Based Calculator!\n")
+
+    while True:
+        print("\nAvailable Operations:")
+        print("1. Addition (+)")
+        print("2. Subtraction (-)")
+        print("3. Multiplication (*)")
+        print("4. Division (/)")
+        print("5. Modulus (%)")
+        print("6. Exponentiation (**)")
+        print("7. Floor Division (//)")
+        print("8. Square Root (sqrt)")
+        print("9. Power (x^y)")
+        print("0. Exit")
